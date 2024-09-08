417072f425f3eb38f370049e1a8e08c2f97a9482
Parsa_Shabanpour <parsash1380@gmail.com>
2024-09-08 19:07:06 +0330

    implement isSubsequence.ts

---
+++ src/topInterview150/twoPointers/isSubsequence.ts
@@ -0,0 +1,11 @@
+export default function isSubsequence(s: string, t: string): boolean {
+  let sIndex = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[sIndex] === t[i]) {
+      sIndex++;
+      if (sIndex === s.length) return true;
+    }
+  }
+
+  return sIndex === s.length;
+}
